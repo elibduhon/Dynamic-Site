ac50e922a0d2103c29fc0eed86b857dc8f946139
elibduhon <elibduhon@gmail.com>
2014-08-03 12:10:41 -0700

    Create css.php

---
+++ css.php
@@ -0,0 +1,41 @@
+	<?php
+/*
+ * CSS:
+ */
+
+
+
+?>
+		
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+		
+		<!-- jQuery CSS -->
+		<link rel="stylesheet" href="jquery-ui.min.css">
+		
+		<!-- FontAwesome -->
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	
+		<style>
+			
+			html,
+			body {
+			  height: 100%;
+			}
+			#wrap {
+				min-height:100%;
+				height: auto;
+				margin: 0 auto -60px;
+				padding: 0 0 60px;
+			}
+			
+			#footer	{
+				height: 60px;
+				background-color: #f5f5f5;
+			}
+			
+		</style>
