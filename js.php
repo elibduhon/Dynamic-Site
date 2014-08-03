c3137a2c193a804a0bee74fc4c365b72a69f790e
elibduhon <elibduhon@gmail.com>
2014-08-03 12:10:15 -0700

    Create js.php

---
+++ js.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Javascript:
+ */
+
+
+
+?>
+
+
+
+		<!-- jQuery-->
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		
+		<!-- jQuery UI -->
+		<script src="jquery-ui.min.js"></script>
+		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		
+		
